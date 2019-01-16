19c319795de788662c27e19a80adda9fb3dc6377
cruzayye <johnnyc010392@gmail.com>
2019-01-15 20:13:08 -0800

    set up app js

--- exercises/lib/app.js
+++ exercises/lib/app.js
@@ -1,29 +1,20 @@
-const http = require('http');
 const { parse } = require('url');
 const bodyParser = require('./bodyParser');
 
+let noteId = 0;
 const notes = {};
+
 module.exports = (req, res) => {
   const url = parse(req.url, true);
   console.log(url.query.name);
   if(req.method === 'POST' && url.pathname === '/note') {
       bodyParser(req)
         .then(body => {
-
+          notes[notId++] = body;
+          res.statusCode = 204;
+          res.end();
         })
-    
   }
 }
 
 
-//////////rick and morty api
-
-module.exports = id => {
-  return request
-    .get('http:/....')
-    .then(res => ({
-      name: res.body.name,
-      species: res.body.species,
-      status: res.body.status
-    }));
-}
