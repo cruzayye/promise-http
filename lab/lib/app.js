a756c30b1449914b5dcad3451bd8b15957f3c8cb
cruzayye <johnnyc010392@gmail.com>
2019-01-16 17:21:35 -0800

    get character by id works

--- lab/lib/app.js
+++ lab/lib/app.js
@@ -1,1 +1,24 @@
-//this module acts as a listener
+
+const { parse } = require('url');
+const {
+  getCharacter,
+  // getCharacters
+} = require('./rickMorty');
+
+module.exports = (req, res) => {
+  const url = parse(req.url, true);
+  if(url.pathname.includes('/character')) {
+    const id = url.pathname.slice(1).split('/')[1];
+    getCharacter(id)
+      .then(character => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(character));
+      })
+      .catch(err => {
+        res.statusCode = 500;
+        res.end(`Error ${err}`);
+      });
+
+
+  }
+};
